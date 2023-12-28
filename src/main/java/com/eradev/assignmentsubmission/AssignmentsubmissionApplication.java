cb581f2d93d9632f6d0af4a9fbd42cbba765ed69
eradev001 <ernazardev@quve.kr>
2023-12-28 17:52:42 +0900

    first commit

---
+++ src/main/java/com/eradev/assignmentsubmission/AssignmentsubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.eradev.assignmentsubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentsubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentsubmissionApplication.class, args);
+	}
+
+}
